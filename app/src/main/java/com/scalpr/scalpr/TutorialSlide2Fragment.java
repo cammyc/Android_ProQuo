a4b0cee6f34ed64f1a0f989f321ec13516fbfa48
Andrew Norris <anorris97@gmail.com>
2017-08-14 00:08:24 -0300

    changed background and attached filters to second slide

--- app/src/main/java/com/scalpr/scalpr/TutorialSlide2Fragment.java
+++ app/src/main/java/com/scalpr/scalpr/TutorialSlide2Fragment.java
@@ -9,12 +9,53 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import io.apptik.widget.MultiSlider;
 
 public class TutorialSlide2Fragment extends Fragment{
+
+    Spinner tutSpin;
+    MultiSlider tutSlide;
+    TextView tutMinPrice, tutMaxPrice;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_tutorial_slide2, container, false);
+
+        tutSpin = (Spinner) rootView.findViewById(R.id.tutorialSpinner);
+        ArrayList<String> options = new ArrayList<String>();
+        options.add("Any");
+        options.add("1");
+        options.add("2");
+        options.add("3");
+        options.add("4+");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getContext(), R.layout.spinner_row, options);
+        adapter.setDropDownViewResource(R.layout.spinner_row);
+        tutSpin.setAdapter(adapter);
+
+
+        tutMinPrice = (TextView) rootView.findViewById(R.id.tutFilterMinPrice);
+        tutMaxPrice = (TextView) rootView.findViewById(R.id.tutFilterMaxPrice);
+
+        tutSlide = (MultiSlider) rootView.findViewById(R.id.rsFilterMinMaxPrice);
+
+        tutSlide.setOnThumbValueChangeListener(new MultiSlider.OnThumbValueChangeListener() {
+            @Override
+            public void onValueChanged(MultiSlider multiSlider, MultiSlider.Thumb thumb, int thumbIndex, int value) {
+                if(thumbIndex == 0){
+                    tutMinPrice.setText("$" + value);
+                }else{
+                    tutMaxPrice.setText("$" + value);
+                }
+            }
+        });
 
         return rootView;
     }
--- app/src/main/java/com/scalpr/scalpr/TutorialViewPagerActivity.java
+++ app/src/main/java/com/scalpr/scalpr/TutorialViewPagerActivity.java
@@ -5,12 +5,14 @@ package com.scalpr.scalpr;
  */
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 
 
 public class TutorialViewPagerActivity extends FragmentActivity {
@@ -26,6 +28,8 @@ public class TutorialViewPagerActivity extends FragmentActivity {
         setContentView(R.layout.activity_tutorial_viewpager);
 
         mPager = (ViewPager) findViewById(R.id.tutorial_viewpager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tutorial_tabs);
+        tabLayout.setupWithViewPager(mPager,true);
         mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
         mPager.setAdapter(mPagerAdapter);
     }
@@ -40,11 +44,16 @@ public class TutorialViewPagerActivity extends FragmentActivity {
         public Fragment getItem(int position) {
 
             switch(position){
-                case 0: return new TutorialSlide1Fragment();
-                case 1: return new TutorialSlide2Fragment();
-                case 2: return new TutorialSlide3Fragment();
-                case 3: return new TutorialSlide4Fragment();
-                default: return new TutorialSlide1Fragment();
+                case 0:
+                    return new TutorialSlide1Fragment();
+                case 1:
+                    return new TutorialSlide2Fragment();
+                case 2:
+                    return new TutorialSlide3Fragment();
+                case 3:
+                    return new TutorialSlide4Fragment();
+                default:
+                    return new TutorialSlide1Fragment();
             }
 
         }
